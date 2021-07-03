250f98f8a72bb2cbb6fc64c3a236d32704bbcc5a
Victor E. Márquez Flores <victor.marquez.flores@gmail.com>
2021-07-02 21:52:43 -0500

    primer cambio eclipse

--- src/main/java/com/mvit/imagen/HomeController.java
+++ src/main/java/com/mvit/imagen/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-/*uno*/
+
 /**
  * Handles requests for the application home page.
  */
